ef9a24bc90a11b5f089d24f361d8767ded30e4df
afeeney9 <afeeney9@iastate.edu>
2023-11-04 13:49:51 -0500

    images and search work

--- Assignments/assignment2/src/Shopping.js
+++ Assignments/assignment2/src/Shopping.js
@@ -1,100 +1,140 @@
 import React, { useState, useEffect } from "react";
 import items from "./selected_products.json";
 
-
-
 const Shop = () => {
-    const [cart, setCart] = useState([]);
-    const [cartTotal, setCartTotal] = useState(0);
+  const [cart, setCart] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+  const [displayItems, setDisplayItems] = useState(items);
+  const [searchVal, setSearchVal] = useState();
 
-    function howManyofThis(id) {
-        let hmot = cart.filter((cartItem) => cartItem.id === id);
-        return hmot.length;
-    }
+  function howManyofThis(id) {
+    let hmot = cart.filter((cartItem) => cartItem.id === id);
+    return hmot.length;
+  }
 
-    const listItems = items.map((el) => (
-        // PRODUCT
-        <div class="row border-top border-bottom" key={el.id}>
-            <div class="row main align-items-center">
-                <div class="col-2">
-                    <img class="img-fluid" src={el.image} />
-                </div>
-                <div class="col">
-                    <div class="row text-muted">{el.title}</div>
-                    <div class="row">{el.category}</div>
-                </div>
-                <div class="col">
-                    <button type="button" variant="light" onClick={() => removeFromCart(el)} > - </button>{" "}
-                    <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
-                </div>
-                <div class="col">
-                    ${el.price} <span class="close">&#10005;</span>{howManyofThis(el.id)}
-                </div>
-            </div>
+  const listItems = displayItems.map((el) => (
+    // PRODUCT
+    <div class="row border-top border-bottom" key={el.id}>
+      <div class="row main align-items-center">
+        <div class="col-2">
+          <img class="img-fluid" src={el.image} />
         </div>
-        ));
+        <div class="col">
+          <div class="row text-muted">{el.title}</div>
+          <div class="row">{el.category}</div>
+        </div>
+        <div class="col">
+          <button
+            type="button"
+            variant="light"
+            onClick={() => removeFromCart(el)}
+          >
+            {" "}
+            -{" "}
+          </button>{" "}
+          <button type="button" variant="light" onClick={() => addToCart(el)}>
+            {" "}
+            +{" "}
+          </button>
+        </div>
+        <div class="col">
+          ${el.price} <span class="close">&#10005;</span>
+          {howManyofThis(el.id)}
+        </div>
+      </div>
+    </div>
+  ));
 
-        const addToCart = (el) => {
-            setCart([...cart, el]);
-        };
+  function handleClick (search) {
+        setDisplayItems(items.filter( (items) => {
+            if (search === "") {return items;}
+            return items.title.toLocaleLowerCase().indexOf(searchVal.toLocaleLowerCase()) > -1;
+        }));
+    
+  }
 
-        const removeFromCart = (el) => {
-            let hardCopy = [...cart];
-            hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
-            setCart(hardCopy);
-        };
+  const addToCart = (el) => {
+    setCart([...cart, el]);
+  };
 
-        const cartItems = cart.map((el) => (
-            <div key={el.id}>
-            <img class="img-fluid" src={el.image} width={150} />
-            {el.title}
-            ${el.price}
-            </div>
-        ));
+  const removeFromCart = (el) => {
+    let hardCopy = [...cart];
+    hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
+    setCart(hardCopy);
+  };
 
-        useEffect(() => {
-            total();
-            }, [cart]);
+  const cartItems = cart.map((el) => (
+    <div key={el.id}>
+      <img class="img-fluid" src={el.image} width={150} />
+      {el.title}${el.price}
+    </div>
+  ));
 
-        const total = () => {
-            let totalVal = 0;
-            for (let i = 0; i < cart.length; i++) {
-                totalVal += cart[i].price;
-            }
-            setCartTotal(totalVal);
-        };
+  useEffect(() => {
+    total();
+  }, [cart]);
+
+  const total = () => {
+    let totalVal = 0;
+    for (let i = 0; i < cart.length; i++) {
+      totalVal += cart[i].price;
+    }
+    setCartTotal(totalVal);
+  };
 
 
-        return (
-            <div>
-            STORE SE/ComS319
-            <div class="card">
-                <div class="row">
-                    {/* HERE, IT IS THE SHOPING CART */}
-                    <div class="col-md-8 cart">
-                        <div class="title">
-                            <div class="row">
-                                <div class="col">
-                                    <h4>
-                                        <b>319 Shopping Cart</b>
-                                    </h4>
-                                </div>
-                                <div class="col align-self-center text-right text-muted">
-                                    Products selected {cart.length}
-                                </div>
-                            </div>
-                        </div>
-                            <div>{listItems}</div>
-                        </div>
-                        <div class ="float-end">
-                            <p class ="mb-0 me-5 d-flex align-items-center">
-                                <span class ="small text-muted me-2">Order total:</span>
-                                <span class ="lead fw-normal">${cartTotal}</span>
-                            </p>
-                        </div>
-                    </div>
+
+
+  const SearchBar = () => (
+    <form class="search">
+          <input
+            onChange={(e) => {setSearchVal(e.target.value)}}
+            value={searchVal}
+            class="form-search"
+            type="text"
+            placeholder="Search"
+            aria-label="Search"
+          ></input>
+          <button class="btn-search" type="submit" onClick={() => {handleClick(searchVal)}}>
+            Search
+          </button>
+          <button class="btn-search" type="submit" onClick={() => {setSearchVal(""); handleClick("")}}>
+            Clear
+          </button>
+        </form>
+  )
+
+  return (
+    <div>
+      <h1>STORE SE/ComS319</h1>
+      <div class="card">
+        <SearchBar/>
+        <div class="row">
+          {/* HERE, IT IS THE SHOPING CART */}
+          <div class="col-md-8 cart">
+            <div class="title">
+              <div class="row">
+                <div class="col">
+                  <h4>
+                    <b>319 Shopping Cart</b>
+                  </h4>
                 </div>
+                <div class="col align-self-center text-right text-muted">
+                  Products selected {cart.length}
+                </div>
+              </div>
             </div>
-            );
-        };
-            export default Shop;
+            <div>{listItems}</div>
+          </div>
+          <div class="float-end">
+            <p class="mb-0 me-5 d-flex align-items-center">
+              <span class="small text-muted me-2">Order total:</span>
+              <span class="lead fw-normal">${cartTotal}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Shop;
